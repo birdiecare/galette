d7b202ec89d53718a5fc9a42be9b08f2d0c33d30
thib92 <thibhenry92@gmail.com>
2018-12-04 18:42:31 +0000

    Fix typings for typedActionCreatorFactory

--- core/src/typed-action-creator/factory.ts
+++ core/src/typed-action-creator/factory.ts
@@ -1,21 +1,20 @@
-type PropertiesResolver = (...args : any[]) => any;
-type ActionCreator = (...args : any[]) => any;
-type ActionCreatorWithType = ActionCreator & {
+export type ActionCreator<A extends any[], R extends any> = ((...args: A) => R & {
+  type: string;
+});
+
+export type ActionCreatorWithType<A extends any[], R extends any> = ActionCreator<A, R> & {
   type: string;
 }
 
-export default function typedActionCreatorFactory(type: string, resolver: PropertiesResolver) : ActionCreatorWithType
+export default function typedActionCreatorFactory<A extends any[], R extends any>(type: string, resolver: (...args: A) => R) : ActionCreatorWithType<A, R>
 {
-  const creator : ActionCreator = (...args : any[]) => {
-    const properties = resolver.apply(null, args);
-
-    return {
-      type,
-      ...properties
-    }
+  const actionCreator = (...args: A) => {
+    const properties = resolver(...args);
+    return Object.assign(properties, {
+      type
+    })
   }
-
-  return Object.assign(creator, {
+  return Object.assign(actionCreator, {
     type
   });
 }
