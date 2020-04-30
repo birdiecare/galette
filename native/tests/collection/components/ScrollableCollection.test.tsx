580fd2058c885f099076284fe63b1bb9750c57d6
Leo Wheelan <leowheelan@hotmail.co.uk>
2020-04-30 18:26:40 +0100

    bug: Pass ScrollableCollection testID down to its InfiniteScrollView (#33)

--- native/tests/collection/components/ScrollableCollection.test.tsx
+++ native/tests/collection/components/ScrollableCollection.test.tsx
@@ -1,48 +1,71 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import {Text} from "react-native";
+import React from "react";
+import renderer from "react-test-renderer";
+import { Text, ScrollView } from "react-native";
 import ScrollableCollection from "../../../src/collection/components/ScrollableCollection";
 import Collection from "../../../src/collection/Collection";
 
-test('it displays only one loading indicator on the first page', () => {
+const noop = () => {};
+const renderRow = () => <Text>Row</Text>;
+
+test("it displays only one loading indicator on the first page", () => {
   const collection = new Collection({
-    items: [
-      {foo: 'bar'},
-    ],
+    items: [{ foo: "bar" }],
     loading: true,
     up_to_page: 1,
-    total_items: 10,
+    total_items: 10
   });
 
-  const tree = renderer.create(
-    <ScrollableCollection
-      onRefresh={() => {}}
-      renderRow={() => <Text>Row</Text>}
-      collection={collection}
-    />
-  ).toJSON();
+  const tree = renderer
+    .create(
+      <ScrollableCollection
+        onRefresh={noop}
+        renderRow={renderRow}
+        collection={collection}
+      />
+    )
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
 
-test('it displays the bottom loading indicator when more than one page has been loaded', () => {
+test("it displays the bottom loading indicator when more than one page has been loaded", () => {
   const collection = new Collection({
-    items: [
-      {foo: 'bar'},
-      {foo: 'baz'},
-    ],
+    items: [{ foo: "bar" }, { foo: "baz" }],
     loading: true,
     up_to_page: 2,
-    total_items: 10,
+    total_items: 10
+  });
+
+  const tree = renderer
+    .create(
+      <ScrollableCollection
+        onRefresh={noop}
+        renderRow={renderRow}
+        collection={collection}
+      />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it("passes a testID prop down to the ScrollView component", () => {
+  const collection = new Collection({
+    items: [{ foo: "bar" }],
+    loading: true,
+    up_to_page: 1,
+    total_items: 10
   });
 
   const tree = renderer.create(
     <ScrollableCollection
-      onRefresh={() => {}}
-      renderRow={() => <Text>Row</Text>}
+      testID="test-id"
+      onRefresh={noop}
+      renderRow={renderRow}
       collection={collection}
     />
-  ).toJSON();
+  );
 
-  expect(tree).toMatchSnapshot();
+  const scrollView = tree.root.findByType(ScrollView);
+  expect(scrollView.props.testID).toBe("test-id");
 });
